5e3eab085f8a2f3769d1d0e907e756b40b5a6c3c
rc-balaji <balajic@drngpit.ac.in>
2023-10-06 01:29:43 +0530

    aaa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+// Lets Start
+
 function App() {
   const [task, setTask] = useState("");
   const [taskList, setTaskList] = useState([{ text: "No Task" }]);
